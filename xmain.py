9a1e2f2bbc65d13d952670c50cfdbb2182848667
MRX - Krypton <36624303+pedrorichil@users.noreply.github.com>
2022-06-30 07:13:42 -0300

    Update xmain.py

--- xmain.py
+++ xmain.py
@@ -2,32 +2,34 @@
 import requests
 import os
 from time import sleep
+from library import *
 msg = " "
-r3 = f'https://api.telegram.org/bot5459930535:AAEdic0b9oB1JUfI8-rITc_A_tlCzldxQy0/sendMessage?chat_id=5164482545&text={msg}'
-
 
 while True:
     url = "https://blaze.com/api/roulette_games/recent"
+    r3 = f'https://api.telegram.org/bot5459930535:AAEdic0b9oB1JUfI8-rITc_A_tlCzldxQy0/sendMessage?chat_id=-1001530806955&text={msg}'
     r2 = requests.get(url).json()
-    cor = r2[0]["color"]
-    cor1 = r2[1]["color"]
-    cor2 = r2[2]["color"]
-    cor3 = r2[3]["color"]
-    cor4 = r2[4]["color"]
-    cor5 = r2[5]["color"]
-    cor6 = r2[6]["color"]
-    cor7 = r2[7]["color"]
-    lista = [cor, cor1, cor2, cor3, cor4, cor4, cor5, cor6, cor7]
+    cor1 = r2[0]["color"]
+    cor2 = r2[1]["color"]
+    cor3 = r2[2]["color"]
+    cor4 = r2[3]["color"]
+    cor5 = r2[4]["color"]
+    cor6 = r2[5]["color"]
+    cor7 = r2[6]["color"]
+    cor8 = r2[7]["color"]
+    lista = [cor1, cor2, cor3, cor4, cor5, cor6, cor7, cor8]
     lista.sort()
     b = lista.count(0)
     v = lista.count(1)
     p = lista.count(2)
-
-    if v >= 6 and p == 2:
-        msg = f"•FX\n Apostar no PRETO\nmartingale:PRETO"
+    cont = red(v)
+    cont2 = black(p)
+    print(f"Branco:{b}\nVermelho:{v}\nPreto:{p}")
+    if v >= 6:
+        msg = f"FX🥋🎯\n Apostar no ⚫️•{cont2}%\nmartingale:⚫️"
         requests.get(r3)
-    if p >= 6 and v == 2:
-        msg = f"•FX\n Apostar no VERMELHO\nmartingale:VERMELHO"
+    elif p >= 6:
+        msg = f"FX🥋🎯\n Apostar no 🔴•{cont}%\nmartingale:🔴"
         requests.get(r3)
     else:
         pass
